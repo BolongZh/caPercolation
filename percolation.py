42123aedb042dfb6b5a1c0451d7a01b7c078086c
Bolong Zheng <saturnino@berkeley.edu>
2021-11-10 16:44:46 -0800

    lots of stuff

--- percolation.py
+++ percolation.py
@@ -1,18 +1,15 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import itertools
-
-# seeding
-np.random.seed(4)
 
 def check_complete(matrix):
     # check if a graph represented by adjacency matrix is complete
-    n  = len(matrix)
+    n = len(matrix)
     for i in range(n):
         for j in range(i + 1, n):
-            if adjacency_matrix[i][j] == 0:
+            if matrix[i][j] == 0:
                 return False
     return True
+
 
 def forward(initial_state, n, h):
     nodes = itertools.combinations([i for i in range(n)], h)
@@ -46,30 +43,23 @@ def forward(initial_state, n, h):
         initial_state[nodeX][nodeY] = 1
 
     # whether the process stabilized
-    return len(toConnect)==0
+    return len(toConnect) == 0
 
-num_sim = 20
-estimated_prob = []
-
-h = 4
-n = 20
-prob_space = np.arange(0,1,0.01)
-for p in prob_space:
-    # mapping a complete graph to a list of coordinates
-    # simulate ER graph generation
-    adjacency_matrix = np.zeros((n, n))
+def trial(num_sim, n, h, p, estimated_prob):
     success = 0
     for _ in range(num_sim):
+        adjacency_matrix = np.zeros((n, n))
         for i in range(n):
             for j in range(i + 1, n):
                 if np.random.random_sample() < p:
                     adjacency_matrix[i][j] = 1
         stabilized = False
         while not stabilized:
-            stabilized = forward(adjacency_matrix,n,h)
+            stabilized = forward(adjacency_matrix, n, h)
         if check_complete(adjacency_matrix):
             success += 1
-    estimated_prob.append(success/num_sim)
+    estimated_prob.append(success / num_sim)
+    return p, success/num_sim
 
-plt.plot(np.arange(0,1,0.01),estimated_prob)
-plt.show()
+
+
